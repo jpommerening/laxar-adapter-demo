ee53a2a45e43bd7417c5bd918c3ef4316f71bb0c
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-01-05 09:32:21 +0100

    Fix styling, build dist for index.html

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,6 +1,8 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
 
+webpack.ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+
 const baseConfig = require( './webpack.config.js' );
 
 module.exports = Object.assign( {}, baseConfig, {
@@ -19,6 +21,23 @@ module.exports = Object.assign( {}, baseConfig, {
       new webpack.optimize.UglifyJsPlugin( {
          compress: { warnings: false },
          sourceMap: true
+      } ),
+      new webpack.ExtractTextPlugin( '[name].css' )
+   ],
+   module: {
+      loaders: baseConfig.module.loaders.map( function( config ) {
+         const test = config.test;
+         const loader = config.loader;
+         if( loader === 'style-loader!css-loader' ) {
+            return {
+               test,
+               loader: webpack.ExtractTextPlugin.extract( 'css-loader' )
+            };
+         }
+         return {
+            test,
+            loader
+         };
       } )
-   ]
+   }
 } );
